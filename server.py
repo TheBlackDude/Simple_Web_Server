95af617b2a330b9eab5cd8847c8b967d4eef8131
Osman Jalloh <omudu45@hotmail.com>
2016-08-17 22:04:57 +0000

    start writing the real Server.py file

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+import StringIO
+import sys
+
+
+class WSGIServer(object):
+    
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 1
+
+    def __init__(self, server_address):
+        # Create a listening socket
+        self.listen_socket = listen_socket = socket.socket(
+            self.address_family, self.socket_type
+        )
+        # Allow to reuse the same address
+        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Bind
+        listen_socket.bind(server_address)
+        # Activate
+        listen_socket.listen(self.request_queue_size)
+        # Get Server host name and port
+        host, port = self.listen_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        # Return headers set by Web Framework/Web application
+        self.headers_set = []
